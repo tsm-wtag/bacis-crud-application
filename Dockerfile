188f760b713b6d68e07d302eb50f12943de3fa27
tsm-wtag <tajmin.sultana@welldev.io>
2024-03-25 15:32:19 +0600

    weit-for sh added

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM python:3.11.8
 
-WORKDIR /Users/mime/Desktop/code/try
+#WORKDIR /Users/mime/Desktop/code/try
+WORKDIR  /app
 
 COPY requirements.txt ./
 
 RUN pip install -r requirements.txt
 
 COPY . .
+RUN chmod +x wait-for.sh
 
 EXPOSE 5000
-
-CMD [ "flask", "run", "--host=0.0.0.0", "--port=5000"]
+RUN apt-get update && apt-get install -y default-mysql-client netcat-traditional && rm -rf /var/lib/apt/lists/*
+#CMD [ "flask", "run", "--host=0.0.0.0", "--port=5000"]
--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from asyncio import tasks
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/Demo'
 
-####@app.route('/')
-##def t():
-  ##return make_response(jsonify({'message': 'test route'}), 200)
-
+try:
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+  #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@flask_db:5432/Demo'
+  #db.init_app(app)
+  print('Connected')
+except Exception as e:
+  print(e)
 db = SQLAlchemy(app)
+
+# @app.route('/')
+# def t():
+#  return make_response(jsonify({'message': 'test route'}), 200)
+
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -39,7 +50,7 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
     return make_response(jsonify({'message': 'user created'}), 201)
-  except e:
+  except:
     return make_response(jsonify({'message': 'error creating user'}), 500)
 
 # get all users
@@ -48,7 +59,7 @@ def get_users():
   try:
     users = User.query.all()
     return make_response(jsonify([user.json() for user in users]), 200)
-  except e:
+  except:
     return make_response(jsonify({'message': 'error getting users'}), 500)
 
 # get a user by id
@@ -59,7 +70,7 @@ def get_user(id):
     if user:
       return make_response(jsonify({'user': user.json()}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+  except:
     return make_response(jsonify({'message': 'error getting user'}), 500)
 
 # update a user
@@ -74,7 +85,7 @@ def update_user(id):
       db.session.commit()
       return make_response(jsonify({'message': 'user updated'}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+  except:
     return make_response(jsonify({'message': 'error updating user'}), 500)
 
 # delete a user
@@ -87,7 +98,7 @@ def delete_user(id):
       db.session.commit()
       return make_response(jsonify({'message': 'user deleted'}), 200)
     return make_response(jsonify({'message': 'user not found'}), 404)
-  except e:
+  except:
     return make_response(jsonify({'message': 'error deleting user'}), 500)
 
 if __name__ == '__main__':
